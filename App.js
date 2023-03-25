4b8a3d30e179ec99b431646e8cc25b268ea271c8
Mariana0416 <sanchezmariana2307@gmail.com>
2023-03-25 17:40:30 -0600

    Cambios nuevos

--- App.js
+++ App.js
@@ -1,85 +1,37 @@
 import React from "react";
-import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  extendTheme,
-  VStack,
-  Box,
-} from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
-import { Platform } from "react-native";
-
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
-
-// extend the theme
-export const theme = extendTheme({ config });
+import { NativeBaseProvider, extendTheme} from 'native-base';
+import SolicitudMantenimiento from "./components/SolicitudMantenimiento";
 
 export default function App() {
-  return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
-    </NativeBaseProvider>
-  );
-}
-
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
-}
+    const theme = extendTheme({
+      colors: {
+        // Add new color
+        tema: {
+          50: '#E3F2F9',
+          100: '#C5E4F3',
+          200: '#A2D4EC',
+          300: '#7AC1E4',
+          400: '#47A9DA',
+          500: '#0088CC',
+          600: '#007AB8',
+          700: '#006BA1',
+          800: '#005885',
+          900: '#003F5E',
+        },
+        // Redefining only one shade, rest of the color will remain same.
+        amber: {
+          400: '#d97706',
+        },
+      },
+      config: {
+        // Changing initialColorMode to 'dark'
+        initialColorMode: 'dark',
+      },
+    });
+  
+    return (
+      <NativeBaseProvider theme={theme}>
+        <SolicitudMantenimiento/>
+      </NativeBaseProvider>
+    );
+  }
---
+++ components/SolicitudMantenimiento.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { FormControl, Input, Heading, Text } from "native-base";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+function solicitudMantenimiento(){
+
+    const [startDate, setStartDate] = React.useState(new Date());
+
+
+    return(
+        <FormControl isRequired isInvalid >
+           <Heading color={"black"} textAlign="center" fontFamily="body">Solicitud de Mantenimiento Correctivo</Heading>
+            <Text  fontSize="md" color={"black"}>Fecha:</Text>
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+            <FormControl.Label>Favorite framework</FormControl.Label>
+            <Input p={2} placeholder="Is it react?" />
+            <FormControl.HelperText>
+                We'll keep this between us.
+            </FormControl.HelperText>
+            <FormControl.ErrorMessage>Something is wrong.</FormControl.ErrorMessage>
+        
+        </FormControl>
+        
+    );
+}
+export default solicitudMantenimiento
